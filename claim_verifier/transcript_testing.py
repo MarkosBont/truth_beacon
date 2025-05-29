e5aa5867021dfe007fb298597e04c2aa158b8750
MarkosBont <markos.bont@gmail.com>
2025-05-29 20:50:47 +0300

    Implemented functions for tokenization and determining which claims need to be fact-checked.

--- claim_verifier/transcript_testing.py
+++ claim_verifier/transcript_testing.py
@@ -1,11 +1,16 @@
-from transcript_verifier import speech_to_text
+from transcript_verifier import speech_to_text, claim_should_be_fact_checked, split_text_into_sentences
 import textwrap
 import warnings
+import nltk
+
+# Downloads the tokenizer data
+nltk.download("punkt")
+nltk.download("punkt_tab")
 
 # Ignoring a warning
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU")
 
-
+"""
 videos = {
     "Elon Musk Deepfake": "/Users/markos/Desktop/elon_deepfake.mp4",
     "Erdogan Deepfake": "/Users/markos/Desktop/erdogan_deepfake.mp4",
@@ -20,3 +25,12 @@ for label, path in videos.items():
     print("Transcript:\n")
     print(textwrap.fill(result["text"], width=80))
     print('\n')
+    """
+
+sentences_list = split_text_into_sentences("The Amazon River is the second longest river in the world, stretching over 6,400 kilometers. It flows through Brazil, Peru, and Colombia before emptying into the Atlantic Ocean. The moon has a diameter of about 3,474 kilometers and takes roughly 27.3 days to orbit Earth. Albert Einstein published his theory of general relativity in 1915, revolutionizing our understanding of gravity. The United States dropped atomic bombs on Hiroshima and Nagasaki in August 1945 during World War II. The human body has 206 bones, with the femur being the longest. The Great Wall of China is more than 21,000 kilometers long and was primarily built to defend against invasions from northern tribes. The Eiffel Tower in Paris was completed in 1889 and stands about 330 meters tall. In 2021, the global population surpassed 7.8 billion people. The euro is the official currency of 20 of the 27 European Union countries.")
+worthy_claims = []
+
+for s in sentences_list:
+    result = claim_should_be_fact_checked(s)
+    print(s +" :" + str(result))
+
--- claim_verifier/transcript_verifier.py
+++ claim_verifier/transcript_verifier.py
@@ -1,10 +1,17 @@
 import whisper
 import os
 import certifi
+import requests
+from nltk.tokenize import sent_tokenize
 
 # Setting SSl certificates in order to load model
 os.environ["SSL_CERT_FILE"] = certifi.where()
 model = whisper.load_model("base")
+
+
+def split_text_into_sentences(text):
+    return sent_tokenize(text)
+
 
 def speech_to_text(video_path):
     """
@@ -25,3 +32,24 @@ def speech_to_text(video_path):
         "text": result["text"],
         "language": result["language"]
     }
+
+
+def claim_should_be_fact_checked(claim):
+
+    api_key = "a9e84fbf35644881b3e378a1af81b21c"
+
+    # Define the endpoint (url) with the claim formatted as part of it, api-key (api-key is sent as an extra header)
+    api_endpoint = f"https://idir.uta.edu/claimbuster/api/v2/score/text/{claim}"
+    request_headers = {"x-api-key": api_key}
+
+    # Sending the GET request to the API and storing the api response
+    api_response = requests.get(url=api_endpoint, headers=request_headers)
+
+    response_json = api_response.json()
+    results = response_json['results']
+    score = results[0]['score']
+    if score > 0.5:
+        return True
+
+    # Returns the JSON payload the API sent back
+    return False
